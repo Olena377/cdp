442d115bac8787c57a1e3244f10e3f97f33f9836
Olena Apoian <Olena_Apoian@epam.com>
2016-05-13 13:09:38 +0300

    changes6

--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,10 @@ var capitalized = require('capitalized-pkg');
 
 document.getElementById('modal-button').addEventListener('click', function() {
   var str = 'my cdp task'
+
+
+
+
+
   document.getElementById('modal-body').innerHTML = capitalized(str);
 });
